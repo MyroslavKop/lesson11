64b55ceef380e845eb0b536e1c3de6a77320edb3
MiroslavKop <kopytovmiroslav@gmail.com>
2022-08-16 18:39:56 +0300

    закончил с задачей

---
+++ script.js
@@ -0,0 +1,54 @@
+"use strick";
+
+// Задача--------------------------------------------------/
+
+// let dayOfBirthday = (date) => {
+//   let currentYear = new Date(date).setFullYear(2022);
+//   let dayOfWeek = new Date(currentYear).getDay();
+//   return dayOfWeek === 0 || dayOfWeek === 6
+//     ? console.log("Happy Birthday, let's celebrate")
+//     : console.log("Happy Birthday, but I need to work");
+// };
+
+// class Person {
+//   constructor(firstName, lastName, age, birthDayDate) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.age = age;
+//     this.birthDayDate = birthDayDate;
+//   }
+//   celebrate() {
+//     console.log("Happy Birthday, let's celebrate");
+//   }
+// }
+
+// class Employee extends Person {
+//   #salary;
+//   constructor(firstName, lastName, age, birthDayDate, salary, jobPosition) {
+//     super(firstName, lastName, age, birthDayDate);
+//     this.#salary = salary;
+//     this.jobPosition = jobPosition;
+//   }
+//   getYearSalary() {
+//     console.log(this.#salary * 12);
+//   }
+//   celebrate() {
+//     dayOfBirthday(this.birthDayDate);
+//   }
+// }
+
+// let person1 = new Person("John", "Johnson", 33, "1989-02-03");
+// console.log(person1);
+// person1.celebrate();
+
+// let employee1 = new Employee(
+//   "Miroslav",
+//   "Kopitov",
+//   25,
+//   "1996-12-17",
+//   7000,
+//   "Developer"
+// );
+// console.log(employee1);
+// employee1.getYearSalary();
+// employee1.celebrate();
